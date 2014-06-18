00eafeba673bd4a5386626d5e53a9e4470876623
agakundle <boermeef@yahoo.com>
2014-06-18 14:19:41 +0200

    created timesheet app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Timesheet::Application.config.secret_key_base = '583a25539fd3fa9387f5559db5a1ab53ecec92cd8588091f347445798cd23bdba2eb799573b13288cae5c5c0971d1167be9438e69cbda1f881811d2b4235673a'
